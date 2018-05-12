d2bb1e569defc50c0bf2768822e0f4e621b30dab
ashtneoi <ashtneoi@gmail.com>
2018-05-12 15:50:30 -0700

    Write part of LinkTreeCursor

--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,13 @@
+use std::collections::HashMap;
 use std::marker::PhantomData;
 
 pub trait Down {
     fn down(&mut self, idx: usize) -> Option<*mut Self>;
+}
+
+pub trait Link {
+    fn name(&self) -> Option<&str>;
+    fn target(&self) -> Option<&str>;
 }
 
 pub struct TreeCursor<'n, N: 'n + Down> {
@@ -45,5 +51,60 @@ impl<'n, N: 'n + Down> TreeCursor<'n, N> {
             self.stack.pop().unwrap();
             true
         }
+    }
+}
+
+type LinkMap<X> = HashMap<String, X>;
+
+// TODO: Is this overly generic?
+pub enum LinkError {
+    DuplicateName,
+    BrokenLink,
+}
+
+pub struct LinkTreeCursor<'n, N: 'n + Down + Link> {
+    tree_cursor: TreeCursor<'n, N>,
+    link_map: LinkMap<*mut N>,
+}
+
+impl<'n, N: 'n + Down + Link> LinkTreeCursor<'n, N> {
+    pub fn new(root: &'n mut N, start: &str) -> Result<Self, LinkError> {
+        let mut c = TreeCursor::new(root);
+        let mut link_map = LinkMap::<*mut N>::new();
+
+        let mut targets = Vec::new();
+
+        loop {
+            let here = c.get_mut();
+            if let Some(name) = match here.name() {
+                    Some(n) => Some(n.to_string()),
+                    None => None,
+            } {
+                if link_map.insert(name, here).is_some() {
+                    return Err(LinkError::DuplicateName);
+                }
+            }
+            if let Some(target) = here.target() {
+                targets.push(target.to_string());
+            }
+
+            while c.down() { }
+            if !c.up() {
+                break;
+            }
+        }
+
+        for target in targets {
+            if !link_map.contains_key(&target) {
+                return Err(LinkError::BrokenLink);
+            }
+        }
+
+        let start_node = match link_map.get(start) {
+            Some(n) => (unsafe { n.as_mut() }).unwrap(),
+            None => return Err(LinkError::BrokenLink),
+        };
+
+        Ok(Self { tree_cursor: TreeCursor::new(start_node), link_map })
     }
 }
